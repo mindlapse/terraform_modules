7ebb8ea42967c6242704befa8a3a81e1c72fa383
Dave MacDonald <mindlapse@gmail.com>
2022-12-31 17:36:57 -0500

    Add prereq dependency on bucket versioning before creating replication config

--- aws/bootstrap/tf_remote_state/aws_s3_bucket_replication_configuration.tf
+++ aws/bootstrap/tf_remote_state/aws_s3_bucket_replication_configuration.tf
@@ -5,6 +5,7 @@ Replicates the terraform .tfstate in ca-central-1 (Montreal) to ap-southeast-2 (
 resource "aws_s3_bucket_replication_configuration" "replication" {
   
     # Must have bucket versioning enabled first
+    depends_on = [module.bucket_backup_region.versioning_id, module.bucket_primary_region.versioning_id]
 
     role   = aws_iam_role.replication.arn
     bucket = module.bucket_primary_region.id
--- aws/bootstrap/tf_remote_state/bucket/_outputs.tf
+++ aws/bootstrap/tf_remote_state/bucket/_outputs.tf
@@ -7,3 +7,8 @@ output "arn" {
     description = "The bucket ARN"
     value = aws_s3_bucket.state.arn
 }
+
+output "versioning_id" {
+    description = "Use as a prerequisite dependency to enabling bucket replication"
+    value = aws_s3_bucket_versioning.versioning.id
+}
